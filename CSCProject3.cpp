6c75993cbb1ec7b4fb0eb2f0ed21eafa9dfb878d
KJMMAD <keira_jmmadrid.@yahoo.com>
2025-10-14 11:42:30 -0400

    Changed code to be closer to the final product after testing the output with just 'cookies' and 'sugar'.

--- CSCProject3.cpp
+++ CSCProject3.cpp
@@ -4,9 +4,10 @@ int main() {
 	int cookies;
 	double sugar, butter, flour;
 
-	std::cout << "Enter the amount of cookies you would like to make:";
+	std::cout << "Enter the amount of cookies you would like to make: ";
 	std::cin >> cookies;
-	std::cout << cookies;
 	sugar = 1.5 / 48 * cookies;
-	std::cout << sugar;
+	butter = 1.0 / 48 * cookies;
+	flour = 2.75 / 48 * cookies;
+	std::cout << "For " << cookies << " cookies, you would need " << sugar << " cups of sugar, " << butter << " cups of butter, and " << flour << " cups of flour.";
 }
