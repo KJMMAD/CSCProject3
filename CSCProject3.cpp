1c7e2bfda1757e0dfcde2e71c06c8f2788396f92
KJMMAD <keira_jmmadrid.@yahoo.com>
2025-10-15 11:45:27 -0400

    Changed 'response' to a string and created bool variable 'check' so that inputs work as intended for the loop to continue.

--- CSCProject3.cpp
+++ CSCProject3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <random>
 using namespace std;
 int main() {
@@ -6,10 +7,11 @@ int main() {
 	mt19937 gen(rd());
 	uniform_int_distribution<> rNum(1, 1000);
 
-	char response = 'y';
+	bool check = true;
 
-	while (response == 'y') {
+	while (check == true){
 		char ch;
+		string response;
 		int x = rNum(gen);
 		int y = rNum(gen);
 		int answer = x + y;
@@ -17,8 +19,13 @@ int main() {
 		cout << "Solve: \n" << x << " + " << y << "\n";
 		cout << "When you're ready, press Enter to see the answer.";
 		cin.get(ch);
-		cout << "The answer was: " << answer << " Continue? [Y]/[N]";
-		cin >> response;
+
+		cout << "The answer was: " << answer << " Continue? [Y]/[N]\n";
+		getline(cin, response);
+
+		if (response != "y") {
+			check = false;
+		}
 		cin.ignore();
 	}
 }
