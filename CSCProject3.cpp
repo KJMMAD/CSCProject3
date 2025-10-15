718a47f2156038b2b008152dda696391e76010fc
KJMMAD <keira_jmmadrid.@yahoo.com>
2025-10-15 09:49:28 -0400

    Added branch with new code for problem 3.

--- CSCProject3.cpp
+++ CSCProject3.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
+using namespace std;
 int main() {
-	int cookies;
-	double sugar, butter, flour;
+	double rate, principal, interest, amount;
+	int t;
 
-	std::cout << "Enter the amount of cookies you would like to make: ";
-	std::cin >> cookies;
-	sugar = 1.5 / 48 * cookies;
-	butter = 1.0 / 48 * cookies;
-	flour = 2.75 / 48 * cookies;
-	std::cout << "For " << std::setprecision(3) << cookies << " cookies, you would need " << sugar << " cups of sugar, " << butter << " cups of butter, and " << flour << " cups of flour.";
-	//I thought it would make the most sense to have the final values round, as trying to get certain precise measurements would be tedious.
+	cout << "To calculate the amount of savings in your account, we need you to provide your account information.\n";
+	cout << "Please enter the current balance of your account first.\n";
+	cin >> principal;
+
+	cout << "Now enter the interest rate of your account.\n";
+	cin >> rate;
+
+	cout << "Lastly, enter the number of times your interest has been compounded.\n";
+	cin >> t;
+
+	amount = principal * pow((1 + (rate / 100) / t), t);
+	interest = amount - principal;
+
+	cout << "Account information:\n";
+	cout << "Interest rate: " << rate << "%\n";
+	cout << "Times compounded: " << t << "\n";
+	cout << "Principal: $" << fixed << setprecision (2) << principal << "\n";
+	cout << "Interest: $" << interest << "\n";
+	cout << "Amount in Savings: $" << amount;
 }
