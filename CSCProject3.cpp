10b268a02d6f8ef8ce529f9717266d4daedcad7d
KJMMAD <keira_jmmadrid.@yahoo.com>
2025-10-15 07:42:33 -0400

    Added code for new branch

--- CSCProject3.cpp
+++ CSCProject3.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
-#include <iomanip>
-
+#include <random>
+using namespace std;
 int main() {
-	int cookies;
-	double sugar, butter, flour;
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> rNum(1, 1000);
 
-	std::cout << "Enter the amount of cookies you would like to make: ";
-	std::cin >> cookies;
-	sugar = 1.5 / 48 * cookies;
-	butter = 1.0 / 48 * cookies;
-	flour = 2.75 / 48 * cookies;
-	std::cout << "For " << std::setprecision(3) << cookies << " cookies, you would need " << sugar << " cups of sugar, " << butter << " cups of butter, and " << flour << " cups of flour.";
-	//I thought it would make the most sense to have the final values round, as trying to get certain precise measurements would be tedious.
+	char response = 'y';
+
+	while (response == 'y') {
+		char ch;
+		int x = rNum(gen);
+		int y = rNum(gen);
+		int answer = x + y;
+		
+		cout << "Solve: \n" << x << " + " << y << "\n";
+		cout << "When you're ready, press Enter to see the answer.";
+		cin.get(ch);
+		cout << "The answer was: " << answer << " Continue? [Y]/[N]";
+		cin >> response;
+		cin.ignore();
+	}
 }
