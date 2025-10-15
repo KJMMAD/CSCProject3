760af402472c1fad16bf1048992d492735ba4b7f
KJMMAD <keira_jmmadrid.@yahoo.com>
2025-10-15 09:52:31 -0400

    Added using namespace std so it would match better with the other programs. Removed any std::. Added fixed flag so numbers round to 2 places after the decimal.

--- CSCProject3.cpp
+++ CSCProject3.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <iomanip>
+using namespace std;
 
 int main() {
 	int cookies;
 	double sugar, butter, flour;
 
-	std::cout << "Enter the amount of cookies you would like to make: ";
-	std::cin >> cookies;
+	cout << "Enter the amount of cookies you would like to make: ";
+	cin >> cookies;
 	sugar = 1.5 / 48 * cookies;
 	butter = 1.0 / 48 * cookies;
 	flour = 2.75 / 48 * cookies;
-	std::cout << "For " << std::setprecision(3) << cookies << " cookies, you would need " << sugar << " cups of sugar, " << butter << " cups of butter, and " << flour << " cups of flour.";
+	cout << "For " << cookies << " cookies, you would need " << fixed << setprecision(2) << sugar << " cups of sugar, " << butter << " cups of butter, and " << flour << " cups of flour.";
 	//I thought it would make the most sense to have the final values round, as trying to get certain precise measurements would be tedious.
 }
